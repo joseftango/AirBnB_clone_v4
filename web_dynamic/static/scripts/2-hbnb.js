0076dba0c3659040f705e8648e91c81ecd8c84f9
joseftango <4279@holbertonschool.com>
2023-05-04 12:01:08 +0100

    2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,13 +1,26 @@
-$(function () {
-    const amenityIds = {};
-  
-    $('.amenities input[type=checkbox]').change(function () {
-      if (this.checked) {
-        amenityIds[$(this).data('name')] = $(this).data('id');
-      } else {
-        delete amenityIds[$(this).data('name')];
-      }
-  
-      $('.amenities h4').text(Object.keys(amenityIds).join(', '));
-    });
+$(document).ready(function () {
+  const amenityIds = {};
+
+  $('.amenities input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenityIds[$(this).data('name')] = $(this).data('id');
+    } else {
+      delete amenityIds[$(this).data('name')];
+    }
+
+    $('.amenities h4').text(Object.keys(amenityIds).join(', '));
   });
+
+  $(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus) {
+      if (data.status == 'OK') {
+        $('div#api_status').addClass('available')
+      }
+      else {
+        $('div#api_status').removeClass('available')
+      }
+    });
+
+  });
+
+});
